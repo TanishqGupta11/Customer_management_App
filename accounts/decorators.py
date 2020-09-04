14e6d8cbae97692d1bbd7ebeec0cada5d79d5998
TanishqGupta11 <tanishqgpt116@gmail.com>
2020-09-04 12:31:02 +0530

    extend users profile with one to one relationship to seperate model that will hold more user details

--- accounts/decorators.py
+++ accounts/decorators.py
@@ -5,7 +5,7 @@ def unauthenticated_user(view_func):
     def wrapper_func(request, *args, **kwargs):
         if request.user.is_authenticated:
             return redirect('home')
-        else:
+        else: 
             return view_func(request, *args, **kwargs)
     return wrapper_func
 
@@ -16,7 +16,7 @@ def allowed_users(allowed_roles=[]):
 
             group = None
             if request.user.groups.exists():
-                group = request.user.groups.all()[0].name
+                group = request.user.groups.all()[0].name  
 
             if group in allowed_roles:
                 return view_func(request, *args, **kwargs)
